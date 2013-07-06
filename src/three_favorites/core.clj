fd047d5dc88920275fb5f4e30ee84d5de0570730
Li Ellis Gallardo <lellisga@gmail.com>
2013-07-06 13:22:34 -0500

    Remove pedestal template functions

--- src/three_favorites/core.clj
+++
@@ -1,6 +0,0 @@
-(ns three-favorites.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/three_favorites/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns three-favorites.core-test
-  (:use clojure.test
-        three-favorites.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
