ceab259dd429d1208138b9a59df2d39fdfe9ffdb
Li Ellis Gallardo <lellisga@gmail.com>
2013-07-06 13:21:16 -0500

    Add Dependencies - Pedestal as the web framework - Comb for the template rendering - Loging dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,23 @@
-(defproject three_favorites "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject three-favorites "0.1.0-SNAPSHOT"
+  :description "Get your three favorite twitter friends tweets"
+  :url "not yet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.pedestal/pedestal.service "0.1.9"]
+                 ;; Remove this line and uncomment the next line to
+                 ;; use Tomcat instead of Jetty:
+                 [io.pedestal/pedestal.jetty "0.1.9"]
+                 [comb "0.1.0"]
+
+                 ;; Logging
+                 [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.2"]
+                 [org.slf4j/jcl-over-slf4j "1.7.2"]
+                 [org.slf4j/log4j-over-slf4j "1.7.2"]]
+
+  :profiles {:dev {:source-paths ["dev"]}}
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :aliases {"run-dev" ["trampoline" "run" "-m" "dev"]}
+  :main ^{:skip-aot true} three-favorites.server)
